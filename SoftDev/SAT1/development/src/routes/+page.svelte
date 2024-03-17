0484d8318b6816e569b4776f5bf330ae83c37d75
idkfelix <felix.coulthard@gmail.com>
2024-03-17 17:43:06 +1100

    vault backup: 2024-03-17 17:43:06

---
+++ SoftDev/SAT1/development/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import CompassClient from "@idkfelix/compass.js";
+  import "@idkfelix/compass.js/src/types/accounts"
+  import { onMount } from "svelte";
+  
+  /** @type {AccountResponse} */
+  let data
+  onMount(async()=>{
+    const client = await CompassClient('mullauna-vic.compass.education','ASP.NET_SessionId=531d2bed-d7b2-4a62-a0b0-80221090818b')
+  })
+</script>
+
+<h1>{data?.name}</h1>
