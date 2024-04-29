1eba36c7037f85eddef8800896f1e9cc4049f76a
idkfelix <felix.coulthard@gmail.com>
2024-04-29 11:51:20 +1000

    vault backup: 2024-04-29 11:51:20

--- SoftDev/SAC3/referance.php
+++ SoftDev/SAC3/referance.php
@@ -1,13 +1,29 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['todo'])) {
+        $task = $_POST['todo'] . PHP_EOL;
+        file_put_contents('todo.csv', $task, FILE_APPEND);
+    }
+    if (isset($_POST['delete'])) {
+        $tasks = file('todo.csv');
+        $updatedTasks = array_filter($tasks, function($task) {
+            return trim($task) !== $_POST['delete'];
+        });
+        file_put_contents('todo.csv', implode('', $updatedTasks));
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Todo Referance</title>
 </head>
 <body>
-  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-    <input type="text">
+  <form method="post" action="/">
+    <input name="todo" type="text">
     <input type="button" value="Create">
   </form>
 </body>
