dd676c03a8b86bac7cc7633d1fabdb82c0b2f890
idkfelix <felix.coulthard@gmail.com>
2024-05-06 23:00:17 +1000

    comments

--- SoftDev/SAC3/app.php
+++ SoftDev/SAC3/app.php
@@ -6,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style/dist.css">
   </head>
-  <body class="p-10 antialiased text-center text-white bg-zinc-800">
+  <body>
+    
     <!-- Create Item Form -->
     <form action="?create" method="post" class="mx-auto section form">
       <h2>Create Todo</h2>
+      <!-- Form Fields -->
       <?=Form::fields([
         'Todo Name'=>[
           'name'=>'name',
@@ -25,15 +27,20 @@
           'type'=>'submit'
         ]
       ])?>
-      <?=Form::errorMsg($_SESSION['error'],'h3')?>
+      <!-- Error Message -->
+      <?=Form::errorMsg(
+        $_SESSION['error'],
+        'h3'
+      )?>
     </form>
-
+    
     <!-- Items List -->
     <div class="mx-auto my-5 section list">
       <h2>Todos</h2>
       <?php foreach($csv->read() as $item):?>
         <?php extract($item); ?>
         <?php $days = date_diff(new DateTime, date_create($date))->d;?>
+        <!-- Item -->
         <form action="?delete" method="post">
           <span><?=$name?></span>
           <span class="flex-grow text-right">
@@ -41,7 +48,9 @@
           </span>
           <input type="checkbox" name="id" value="<?=$id?>" onClick="this.form.submit()">
         </form>
+        <!-- /Item -->
       <?php endforeach?>
     </div>
+
   </body>
 </html>
--- SoftDev/SAC3/main.php
+++ SoftDev/SAC3/main.php
@@ -1,44 +1,65 @@
 <?php
+// Start PHP Session
 session_start();
+// Auto Load Classes from './lib'
 spl_autoload_register(fn($class)=>include_once("./lib/$class.php"));
-$csv = new CSV('./data/items.csv');
+// Initialise Classes
+$csv = new CSV('items.csv');
 $val = new Validate($_POST);
 
-// Classify Request Method
+
+/** 
+ * Create New Item 
+ * @param string[] $data
+ */
+function handleCreate(array $data):void {
+  global $csv, $val;
+  // Verify Fields
+  $val->fieldRules([
+    'name'=>'required|type:string',
+    'date'=>'required|type:string|date',
+  ]);
+  // Verify Date Range
+  $val->dateRange('date',['now',null]);
+  // Create Item
+  $csv->append([
+    'id'=>uniqid(),
+    ...($data)
+  ]); 
+}
+
+/** 
+ * Delete Item with ID
+ * @param string[] $data
+ */
+function handleDelete(array $data):void {
+  global $csv, $val;
+  // Verify Fields
+  $val->fieldRules([
+    'id'=>'required|type:string'
+  ]);
+  // Delete Item
+  $csv->remove($data['id']);
+}
+
+
+// Handle Request Methods
 switch($_SERVER['REQUEST_METHOD']){
   case 'POST':
     try {
+      // Match Request Query
       match($_SERVER['QUERY_STRING']){
-        'create'=>handleCreate(),
-        'delete'=>handleDelete(),
+        'create'=>handleCreate($_POST),
+        'delete'=>handleDelete($_POST),
       };
     } catch(Exception $e){
+      // Set Error Message on Faliure
       $_SESSION['error'] = $e->getMessage();
     }  
     // Send Client back to App
     header("Location: {$_SERVER['PHP_SELF']}");
     break;
+  // Default Show App
   default:
     include_once('app.php');
 };
-
-/** Create New Item */
-function handleCreate(){
-  global $_POST, $csv, $val;
-  $val->fieldRules([
-    'name'=>'required|type:string',
-    'date'=>'required|type:string|date',
-  ]);
-  $val->dateRange('date',['now',null]);
-  $csv->append([
-    'id'=>uniqid(),
-    ...($_POST)
-  ]); 
-}
-
-/** Delete Item with Id */
-function handleDelete(){
-  global $_POST, $csv, $val;
-  $val->fieldRules(['id'=>'required|type:string']);
-  $csv->remove($_POST['id']);
-}
